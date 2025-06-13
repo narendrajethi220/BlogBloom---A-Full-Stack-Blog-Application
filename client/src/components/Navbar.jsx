2cb7faa21271ec1ee7d6598d51be556c47a4dff8
Narendra Jethi <narendrajethi220@gmail.com>
2025-06-13 12:35:07 +0530

    fix: resolved navbar logo navigation to redirect to homepage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,33 @@
-import { assets } from "../assets/assets"
+import { assets } from "../assets/assets";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   return (
     // <div className="flex justify-between items-center py-5 mx-8 sm:mx-10 xl:mx-30">
-      <div className="fixed top-0 left-0 w-[90%] bg-white/90 backdrop-blur-sm z-[99] border-b border-gray-200 flex justify-between items-center py-5 mx-8 sm:mx-10 xl:mx-20 ">
-        <div className="flex justify-between items-center cursor-pointer">
-            <img onClick={()=>navigate('/')} src={assets.IconBlog} alt="Logo" className="w-10 sm:w-12 mr-[-.3rem]"/>
-            <p className="text-zinc-800 text-3xl font-bold">logBloom</p>
-        </div>
-        <button onClick={()=>navigate('/admin')} className="flex justify-between items-center gap-2 rounded-full cursor-pointer bg-primary text-white px-4 py-1 hover:bg-dark transition duration-300 ease-in-out">Login
-           <FaArrowRightLong />
-        </button>
+    <div className="fixed top-0 left-0 w-[90%] bg-white/90 backdrop-blur-sm z-[99] border-b border-gray-200 flex justify-between items-center py-5 mx-8 sm:mx-10 xl:mx-20 ">
+      <div
+        onClick={() => navigate("/")}
+        className="flex justify-between items-center cursor-pointer"
+      >
+        <img
+          src={assets.IconBlog}
+          alt="Logo"
+          className="w-10 sm:w-12 mr-[-.3rem]"
+        />
+        <p className="text-zinc-800 text-3xl font-bold">logBloom</p>
+      </div>
+      <button
+        onClick={() => navigate("/admin")}
+        className="flex justify-between items-center gap-2 rounded-full cursor-pointer bg-primary text-white px-4 py-1 hover:bg-dark transition duration-300 ease-in-out"
+      >
+        Login
+        <FaArrowRightLong />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar; 
+export default Navbar;
