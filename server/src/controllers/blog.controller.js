5eb0426d4c6b7bcd92e322adf9aefc281a39b405
Narendra Jethi <narendrajethi220@gmail.com>
2025-06-25 14:36:37 +0530

    completed blog APIs - add, fetch all, fetch by ID, delete by ID, and toggle publish status of blog

--- server/src/controllers/blog.controller.js
+++ server/src/controllers/blog.controller.js
@@ -55,3 +55,89 @@ export const addBlog = async (req, res) => {
     });
   }
 };
+
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ isPublished: true });
+    res.status(200).json({
+      success: true,
+      message: "Successfully fetched blogs",
+      blogs,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+export const getBlogById = async (req, res) => {
+  try {
+    const { blogId } = req.params;
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res.status(404).json({
+        success: false,
+        message: "No Blog Found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Successfully fetched blog",
+      blog,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+export const deleteBlogById = async (req, res) => {
+  try {
+    const { blogId } = req.body;
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res.status(404).json({
+        success: false,
+        message: "No Blog Found",
+      });
+    }
+    await Blog.findByIdAndDelete(blogId);
+    res.status(200).json({
+      success: true,
+      message: "Blog Deleted Successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+export const togglePublish = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const blog = await Blog.findById(id);
+    if (!blog) {
+      return res.status(404).json({
+        success: false,
+        message: "No Blog Found",
+      });
+    }
+    blog.isPublished = !blog.isPublished;
+    await blog.save();
+    res.status(201).json({
+      success: true,
+      message: "Blog Status Updated",
+    });
+  } catch (err) {
+    res.status(200).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
--- server/src/routes/blog.routes.js
+++ server/src/routes/blog.routes.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { addBlog } from "../controllers/blog.controller.js";
+import {
+  addBlog,
+  deleteBlogById,
+  getAllBlogs,
+  getBlogById,
+  togglePublish,
+} from "../controllers/blog.controller.js";
 import upload from "../middlewares/multer.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const blogRouter = express.Router();
 
 blogRouter.post("/blog", authMiddleware, upload.single("image"), addBlog);
+blogRouter.get("/blog", getAllBlogs);
+blogRouter.get("/blog/:blogId", getBlogById);
+blogRouter.delete("/blog", authMiddleware, deleteBlogById);
+blogRouter.post("/blog/toggle-publish", authMiddleware, togglePublish);
 
 export default blogRouter;
